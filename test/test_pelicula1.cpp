e4503b426611fca68922a4f868b35c71bf56df99
mgp365 <A00840918@tec.mx>
2025-06-14 17:45:47 -0600

    Create test_pelicula1.cpp

---
+++ test/test_pelicula1.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "../pelicula.h"
+
+TEST(PeliculaTest, ObtenerCalificacion) {
+    Pelicula peli(101, "Interestelar", "Sci-Fi", 2.5, 4.5);
+    EXPECT_DOUBLE_EQ(peli.obtenerCalificacion(), 4.5);
+}
+
+TEST(PeliculaTest, MostrarInfoFiltrada) {
+    Pelicula peli(102, "Drama", "Drama", 2.0, 4.2);
+    testing::internal::CaptureStdout();
+    peli.mostrarInfoFiltrada(4.2, 4.0);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Nombre: Drama"), std::string::npos);
+}
