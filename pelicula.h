cae86bd24a0c9a59f718fb141f9ac3d4afa40ce9
mgp365 <A00840918@tec.mx>
2025-06-15 09:49:17 -0600

    Update pelicula.h

--- pelicula.h
+++ pelicula.h
@@ -9,6 +9,15 @@
 
 #include "videos.h"
 
+/**
+ * @brief Clase que representa una película, hereda de Videos
+ * 
+ * Esta clase implementa las funcionalidades específicas de una película,
+ * extendiendo las características básicas de la clase Videos.
+ * 
+ * @author Mariana Guerrero Pérez
+ * @date 27/05/2025
+ */
 class Pelicula : public Videos {
     public:
     Pelicula(int id, const string& nombre, const string& genero, float duracion, double calificacion);
