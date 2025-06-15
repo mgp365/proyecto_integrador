9b1b23f09d7bf6016418b81eb384d916a22c496c
mgp365 <A00840918@tec.mx>
2025-06-15 02:13:28 -0600

    Update test_serie.cpp

--- test/test_serie.cpp
+++ test/test_serie.cpp
@@ -22,3 +22,17 @@ TEST(SerieTest, ObtenerSerie) {
     Serie serie(203, "Game of Thrones", 4.7, "Fantasía", 1.2, 8);
     EXPECT_EQ(serie.obtenerSerie(), "Game of Thrones");
 }
+
+TEST(SerieTest, GuardarEnArchivo) {
+    Serie serie(205, "The Office", 4.1, "Comedia", 0.5, 9);
+    std::ostringstream archivo;
+    serie.guardarEnArchivo(archivo);
+    std::string contenido = archivo.str();
+    
+    EXPECT_NE(contenido.find("205"), std::string::npos);
+    EXPECT_NE(contenido.find("The Office"), std::string::npos);
+    EXPECT_NE(contenido.find("9"), std::string::npos);
+    EXPECT_NE(contenido.find("Comedia"), std::string::npos);
+    EXPECT_NE(contenido.find("0.5"), std::string::npos);
+    EXPECT_NE(contenido.find("4.1"), std::string::npos);
+}
