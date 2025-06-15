94842e963c0d1510277fd3913107e89f67d2312d
mgp365 <A00840918@tec.mx>
2025-06-15 00:50:51 -0600

    Update test_pelicula1.cpp

--- test/test_pelicula1.cpp
+++ test/test_pelicula1.cpp
@@ -6,10 +6,10 @@ TEST(PeliculaTest, ObtenerCalificacion) {
     EXPECT_DOUBLE_EQ(peli.obtenerCalificacion(), 4.5);
 }
 
-//TEST(PeliculaTest, MostrarInfoFiltrada) {
-//    Pelicula peli(102, "Drama", "Drama", 2.0, 4.2);
-//    testing::internal::CaptureStdout();
-//    peli.mostrarInfoFiltrada(4.2, 4.0);
-//    std::string output = testing::internal::GetCapturedStdout();
-//    EXPECT_NE(output.find("Nombre: Drama"), std::string::npos);
-//}
+TEST(PeliculaTest, MostrarInfoFiltrada) {
+    Pelicula peli(102, "Drama", "Drama", 2.0, 4.2);
+    testing::internal::CaptureStdout();
+    peli.mostrarInfoFiltrada(4.2, 4.0);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Nombre: Drama"), std::string::npos);
+}
