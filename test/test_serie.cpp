d0dc31547dfc62802743c2bbe3136a9a4218d058
mgp365 <A00840918@tec.mx>
2025-06-15 01:27:14 -0600

    Update test_serie.cpp

--- test/test_serie.cpp
+++ test/test_serie.cpp
@@ -7,3 +7,18 @@ TEST(SerieTest, Constructor) {
     EXPECT_DOUBLE_EQ(serie.obtenerCalificacion(), 4.9);
     EXPECT_EQ(serie.obtenerEpisodio(), 5);
 }
+
+TEST(SerieTest, ObtenerCalificacion) {
+    Serie serie(201, "Breaking Bad", 4.9, "Drama", 1.0, 5);
+    EXPECT_DOUBLE_EQ(serie.obtenerCalificacion(), 4.9);
+}
+
+TEST(SerieTest, ObtenerEpisodio) {
+    Serie serie(202, "Friends", 4.2, "Comedia", 0.5, 10);
+    EXPECT_EQ(serie.obtenerEpisodio(), 10);
+}
+
+TEST(SerieTest, ObtenerSerie) {
+    Serie serie(203, "Game of Thrones", 4.7, "Fantasía", 1.2, 8);
+    EXPECT_EQ(serie.obtenerSerie(), "Game of Thrones");
+}
