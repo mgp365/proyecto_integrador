32c0cc831d014525e967b0f36a973b41f422e141
mgp365 <A00840918@tec.mx>
2025-06-15 01:49:16 -0600

    Update test_videos.cpp

--- test/test_videos.cpp
+++ test/test_videos.cpp
@@ -17,3 +17,24 @@ TEST(VideosTest, PolimorfismoObtenerCalificacion) {
     delete pelicula;
     delete serie;
 }
+
+TEST(VideosTest, PolimorfismoObtenerTitulo) {
+    Videos* pelicula = new Pelicula(303, "Película Test", "Drama", 2.5, 4.2);
+    Videos* serie = new Serie(304, "Serie Test", 4.1, "Comedia", 1.0, 5);
+    
+    EXPECT_EQ(pelicula->obtenerTitulo(), "Película Test");
+    EXPECT_EQ(serie->obtenerTitulo(), "Serie Test");
+    
+    delete pelicula;
+    delete serie;
+}
+
+TEST(VideosTest, AgregarCalificacion_Pelicula) {
+    Videos* pelicula = new Pelicula(305, "Test", "Acción", 2.0, 4.0);
+    std::ostringstream archivo;
+    
+    pelicula->agregarCalificacion(6.0, archivo);
+    EXPECT_DOUBLE_EQ(pelicula->obtenerCalificacion(), 5.0); // (4.0 + 6.0) / 2
+    
+    delete pelicula;
+}
