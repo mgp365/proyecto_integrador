79a2c1001ebeb392273b15b1db8dee2f22ab0d46
mgp365 <A00840918@tec.mx>
2025-06-15 01:39:39 -0600

    Update test_pelicula.cpp

--- test/test_pelicula.cpp
+++ test/test_pelicula.cpp
@@ -16,3 +16,17 @@ TEST(PeliculaTest, ObtenerTitulo) {
     Pelicula peli(102, "El Padrino", "Drama", 3.0, 4.8);
     EXPECT_EQ(peli.obtenerTitulo(), "El Padrino");
 }
+
+TEST(SerieTest, GuardarEnArchivo) {
+    Serie serie(205, "The Office", 4.1, "Comedia", 0.5, 9);
+    std::ostringstream archivo;
+    serie.guardarEnArchivo(archivo);
+    std::string contenido = archivo.str();
+    
+    EXPECT_NE(contenido.find("205"), std::string::npos);
+    EXPECT_NE(contenido.find("The Office"), std::string::npos);
+    EXPECT_NE(contenido.find("9"), std::string::npos);
+    EXPECT_NE(contenido.find("Comedia"), std::string::npos);
+    EXPECT_NE(contenido.find("0.5"), std::string::npos);
+    EXPECT_NE(contenido.find("4.1"), std::string::npos);
+}
