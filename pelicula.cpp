eea2c4394d1f4b35cf81572b2f1e3276220e1402
mgp365 <A00840918@tec.mx>
2025-06-15 09:17:13 -0600

    Update pelicula.cpp

--- pelicula.cpp
+++ pelicula.cpp
@@ -1,9 +1,6 @@
 /*
- * Copyright (C) 2025 Tec de Monterrey
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Copyright (c) 2024 Mariana Guerrero Pérez
+ * Tecnológico de Monterrey
  */
 
 #include <iostream>
