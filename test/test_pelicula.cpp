9bb0464d5e20e0dea84d69ceb8ff1414a4d84be7
mgp365 <A00840918@tec.mx>
2025-06-15 02:35:05 -0600

    Update test_pelicula.cpp

--- test/test_pelicula.cpp
+++ test/test_pelicula.cpp
@@ -48,3 +48,29 @@ TEST(PeliculaTest, GuardarEnArchivo) {
     EXPECT_NE(contenido.find("3.2"), std::string::npos);
     EXPECT_NE(contenido.find("4.6"), std::string::npos);
 }
+
+TEST(PeliculaTest, MostrarInfoFiltrada_CalificacionMayor) {
+    Pelicula peli(102, "Drama", "Drama", 2.0, 4.2);
+    testing::internal::CaptureStdout();
+    peli.mostrarInfoFiltrada(4.2, 4.0);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Nombre: Drama"), std::string::npos);
+    EXPECT_NE(output.find("Género: Drama"), std::string::npos);
+    EXPECT_NE(output.find("ID: 102"), std::string::npos);
+}
+
+TEST(PeliculaTest, MostrarInfoFiltrada_CalificacionMenor) {
+    Pelicula peli(104, "Comedia", "Comedia", 1.5, 3.0);
+    testing::internal::CaptureStdout();
+    peli.mostrarInfoFiltrada(3.0, 4.0);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_TRUE(output.empty());
+}
+
+TEST(PeliculaTest, MostrarInfoFiltrada_CalificacionIgual) {
+    Pelicula peli(105, "Thriller", "Thriller", 2.2, 4.0);
+    testing::internal::CaptureStdout();
+    peli.mostrarInfoFiltrada(4.0, 4.0);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_TRUE(output.empty());
+}
