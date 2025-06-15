ada88ccb397f3e76f42af01b7ba69387d1f49400
mgp365 <A00840918@tec.mx>
2025-06-15 01:45:01 -0600

    Update test_videos.cpp

--- test/test_videos.cpp
+++ test/test_videos.cpp
@@ -1,1 +1,19 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include "videos.h"
+#include "pelicula.h"
+#include "serie.h"
 
+// Usamos las clases derivadas para probar la funcionalidad de la clase base Videos
+// ya que Videos es una clase base con métodos virtuales
+
+TEST(VideosTest, PolimorfismoObtenerCalificacion) {
+    Videos* pelicula = new Pelicula(301, "Test Movie", "Acción", 2.0, 4.0);
+    Videos* serie = new Serie(302, "Test Series", 3.5, "Thriller", 1.5, 3);
+    
+    EXPECT_DOUBLE_EQ(pelicula->obtenerCalificacion(), 4.0);
+    EXPECT_DOUBLE_EQ(serie->obtenerCalificacion(), 3.5);
+    
+    delete pelicula;
+    delete serie;
+}
