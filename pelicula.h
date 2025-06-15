8134303bd11e49437471d4129de0cb02752ac516
mgp365 <A00840918@tec.mx>
2025-06-15 09:07:29 -0600

    Update pelicula.h

--- pelicula.h
+++ pelicula.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2025 Tec de Monterrey
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #ifndef PELICULA_H
 #define PELICULA_H
 
