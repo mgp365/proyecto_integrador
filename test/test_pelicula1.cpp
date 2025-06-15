f8ae2ff2e13318239a3d32ac227f24cab5d8998e
mgp365 <A00840918@tec.mx>
2025-06-15 01:08:36 -0600

    Update test_pelicula1.cpp

--- test/test_pelicula1.cpp
+++ test/test_pelicula1.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
 #include "pelicula.h"
 
+TEST(PeliculaTest, Constructor) {
+    Pelicula peli(101, "Interestelar", "Sci-Fi", 2.5, 4.5);
+    EXPECT_EQ(peli.obtenerTitulo(), "Interestelar");
+    EXPECT_DOUBLE_EQ(peli.obtenerCalificacion(), 4.5);
+}
+
 TEST(PeliculaTest, ObtenerCalificacion) {
     Pelicula peli(101, "Interestelar", "Sci-Fi", 2.5, 4.5);
     EXPECT_DOUBLE_EQ(peli.obtenerCalificacion(), 4.5);
